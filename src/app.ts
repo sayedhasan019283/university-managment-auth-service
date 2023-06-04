a1b24d8c628c6bbc6014e0ef6c78ca7d00ab2377
sayed <sayedhasan019283@gmail.com>
2023-06-04 17:30:09 +0600

    adding service and utils

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
+import usersRouter from './app/modules/users/users.route'
 const app: Application = express()
 
 app.use(cors())
@@ -7,6 +8,9 @@ app.use(cors())
 // parser
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
+//application route
+app.use('/api/v1/users/', usersRouter.router)
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!')
